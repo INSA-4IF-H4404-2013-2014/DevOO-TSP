297221d9743dbaea02dde72f790839a38c8158d8
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-27 13:23:19 +0100

    Adds Intellij IDEA project

---
+++ intellij/src/demo/uml/Hello.java
@@ -0,0 +1,11 @@
+package demo.uml;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gabadie
+ * Date: 27/11/13
+ * Time: 13:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+}
