32daefc76c71476f21c34ba9870c2c222f9f15b2
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-02 22:00:36 +0100

    Completes View.MapPanel.MapPanel's constructor

--- ids/src/View/MapPanel/MapPanel.java
+++ ids/src/View/MapPanel/MapPanel.java
@@ -1,7 +1,10 @@
 package View.MapPanel;
 
+import Model.City.Graph;
+
 import javax.swing.*;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +15,23 @@ import java.util.List;
  */
 public class MapPanel extends JPanel {
 
-    private List<Node> nodes;
-    private List<Arc> arcs;
+    /** associated model graph */
+    private Graph modelGraph;
 
+    /** nodes map */
+    private Map<Integer,Node> nodes;
 
+    /** arcs map */
+    private Map<Integer,Map<Integer,Arc>> arcs;
+
+    /**
+     * Constructor
+     * @param modelGraph the associated model graph
+     */
+    public MapPanel(Graph modelGraph) {
+        this.modelGraph = modelGraph;
+        this.nodes = new HashMap<Integer,Node>();
+        this.arcs = new HashMap<Integer,Map<Integer,Arc>>();
+    }
 
 }
