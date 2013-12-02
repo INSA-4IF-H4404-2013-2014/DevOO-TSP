862cf6357b6179dff3f88832b040ecccc1150cda
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-12-02 22:09:00 +0100

    Completes initial version of top JMenuBar.

--- ids/src/View/MainWindow/TopMenuBar.java
+++ ids/src/View/MainWindow/TopMenuBar.java
@@ -1,6 +1,9 @@
 package View.MainWindow;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 /**
  * @author H4404 - ABADIE Guillaume, BUISSON Nicolas, CREPET Louise, DOMINGUES Rémi, MARTIN Aline, WETTERWALD Martin
@@ -9,11 +12,6 @@ import javax.swing.*;
  * This class is the JMenuBar at the top of the main window
  */
 public class TopMenuBar extends JMenuBar{
-    private JMenu file = new JMenu("Fichier");
-    private JMenu edit = new JMenu("Éditer");
-    private JMenu view = new JMenu("Affichage");
-    private JMenu help = new JMenu("?");
-
     public TopMenuBar() {
         createFileMenu();
         createEditMenu();
@@ -22,18 +20,38 @@ public class TopMenuBar extends JMenuBar{
     }
 
     private void createFileMenu() {
+        JMenu file = new JMenu("Fichier");
+
+        // Exit button
+        JMenuItem fileExit = new JMenuItem("Quitter", new ImageIcon(getClass().getResource("../../door_out.png")));
+        fileExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        fileExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+
+        file.add(fileExit);
+
         add(file);
     }
 
     private void createEditMenu() {
+        JMenu edit = new JMenu("Éditer");
+        edit.setEnabled(false);
         add(edit);
     }
 
     private void createViewMenu() {
+        JMenu view = new JMenu("Affichage");
+        view.setEnabled(false);
         add(view);
     }
 
     private void createHelpMenu() {
+        JMenu help = new JMenu("?");
+        help.setEnabled(false);
         add(help);
     }
 }
