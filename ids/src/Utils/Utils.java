f5a2a2c206869c5b0534141263e81a0980cbca50
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-02 15:58:15 +0100

    Implements Utils.parsePositiveFloat(String)

---
+++ ids/src/Utils/Utils.java
@@ -0,0 +1,42 @@
+package Utils;
+
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gabadie
+ * Date: 02/12/13
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utils {
+
+    public static float parsePositiveFloat(String string) throws UtilsException {
+        int stringLength = string.length();
+        float value = 0.0f;
+        float divisor = 1.0f;
+        boolean end = false;
+
+        for(int i = 0; i < stringLength; i++) {
+            char c = string.charAt(i);
+
+            if (!end && c == ',') {
+                end = true;
+            }
+            else if (c >= '0' && c <= '9') {
+                if (end) {
+                    divisor *= 10.0f;
+                }
+
+                value = value * 10.0f + (float)(c - '0');
+            }
+            else {
+                throw new UtilsException("positive parsing error");
+            }
+        }
+
+        return value / divisor;
+    }
+
+
+}
