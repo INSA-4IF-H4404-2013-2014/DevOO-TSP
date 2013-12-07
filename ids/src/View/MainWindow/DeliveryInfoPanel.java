a3b4ff87ab38d0378118da13077f0e1f7533418f
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-12-07 10:55:49 +0100

    Splits DeliveryInfoPanel into two parts: top and bottom.

--- ids/src/View/MainWindow/DeliveryInfoPanel.java
+++ ids/src/View/MainWindow/DeliveryInfoPanel.java
@@ -23,5 +23,33 @@ public class DeliveryInfoPanel extends JPanel {
 
     public DeliveryInfoPanel() {
         setPreferredSize(new Dimension((int)(MainWindow.DEFAULT_WIDTH*DEFAULT_WIDTH_RATIO), 0));
+        setLayout(new GridLayout(2,1));
+        add(createTopDeliveryInfoPanel());
+        add(createBottomDeliveryInfoPanel());
+    }
+
+    /**
+     * Creates top part of the delivery info panel (this is the top most right part
+     * in the main window).
+     *
+     * @return the JPanel
+     */
+    private JPanel createTopDeliveryInfoPanel()
+    {
+        JPanel topDeliveryInfoPanel = new JPanel();
+        topDeliveryInfoPanel.setBackground(Color.GRAY);
+        return topDeliveryInfoPanel;
+    }
+
+    /**
+     * Creates the bottom part of the delivery info panel (this is the bottom most right part
+     * in the main window).
+     * @return
+     */
+    private JPanel createBottomDeliveryInfoPanel()
+    {
+        JPanel bottomDeliveryInfoPanel = new JPanel();
+        bottomDeliveryInfoPanel.setBackground(Color.LIGHT_GRAY);
+        return bottomDeliveryInfoPanel;
     }
 }
