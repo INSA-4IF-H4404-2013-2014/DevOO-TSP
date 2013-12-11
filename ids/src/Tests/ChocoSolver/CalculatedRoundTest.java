12e2275768a25ee171f10b2f7adc4378af8e09e9
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2013-12-11 10:51:27 +0100

    Implemented tests for CalculatedRound

--- ids/src/Tests/ChocoSolver/CalculatedRoundTest.java
+++ ids/src/Tests/ChocoSolver/CalculatedRoundTest.java
@@ -1,5 +1,8 @@
 package Tests.ChocoSolver;
 
+import Model.City.Network;
+import Model.City.Node;
+import Model.Delivery.Client;
 import org.junit.Test;
 
 /**
@@ -14,5 +17,22 @@ public class CalculatedRoundTest {
     @Test
     public void testEstimatedSchedule() {
         // TODO: test this function once computeRound() is ready
+
+        Network myNetwork = new Network();
+
+        myNetwork.createNode(1, 5, 5);
+        myNetwork.createNode(2, 21, 10);
+        myNetwork.createNode(3, 30, 60);
+
+        myNetwork.createStreet("1");
+        myNetwork.createStreet("2");
+        myNetwork.createStreet("3");
+
+        myNetwork.findStreet("1").createArc(myNetwork.findNode(1), myNetwork.findNode(2), 10, 100);
+        myNetwork.findStreet("2").createArc(myNetwork.findNode(2), myNetwork.findNode(3), 10, 200);
+        myNetwork.findStreet("3").createArc(myNetwork.findNode(3), myNetwork.findNode(1), 10, 100);
+
+        Client client = new Client("Toto");
+
     }
 }
