c134ba64c2ef7016ac017005d40315e7fb948e79
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-12-12 15:05:33 +0100

    Enhances delivery name display in 'DeliveryListPanel'.

--- ids/src/Model/Delivery/Delivery.java
+++ ids/src/Model/Delivery/Delivery.java
@@ -128,6 +128,6 @@ public class Delivery {
     }
 
     public String toString() {
-        return String.valueOf(id) + " - " + address;
+        return "Client " + client + " @ " + address;
     }
 }
