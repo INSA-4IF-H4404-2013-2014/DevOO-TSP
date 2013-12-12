INSA-4IF-H4404-2013-2014/DevOO-TSP

commit 4a1e04e7ad0a193f0f47b0b61c67c3ae2f5fa115

*unstable* debogging with remi and *stable* making binding

ids/src/Controller/MainWindowController.java
@@ -89,6 +89,10 @@ public class MainWindowController implements NodeListener {
             }
         }
         computeRound(this.getMainWindow().getNetwork(), this.getMainWindow().getRound());
+        mainWindow.getRightPanel().getRoundPanel().emptyFields();
+        mainWindow.getRightPanel().getDeliveryInfoPanel().emptyFields();
+        mainWindow.getMapPanel().setSelectedNode(null);
+        mainWindow.getRightPanel().getRoundPanel().fillRoundPanel(this.mainWindow.getCalculatedRound());
     }
 
     /**
@@ -164,7 +168,7 @@ public class MainWindowController implements NodeListener {
     /**
      * Compute the actual round to find the best delivery plan. Calls the view to print it if it has been found.
      */
-    public void computeRound(Network network, Round round) {
+    public int computeRound(Network network, Round round) {
         ChocoGraph graph = new ChocoGraph(network, round);
 
         TSP tsp = solveTsp(graph, 100);
@@ -173,10 +177,13 @@ public class MainWindowController implements NodeListener {
         if((solutionState == SolutionState.SOLUTION_FOUND) || (solutionState == SolutionState.OPTIMAL_SOLUTION_FOUND)) {
             CalculatedRound calculatedRound = createCalculatedRound(tsp, graph);
             mainWindow.getMapPanel().setRound(calculatedRound);
+            return 0;
         } else {
-            JOptionPane.showMessageDialog(mainWindow, "Aucune trajet trouvé", "Erreur", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(mainWindow, "Aucun trajet trouvé", "Erreur", JOptionPane.ERROR_MESSAGE);
+            return 1;
         }
     }
+
 
     @Override
     public void nodeClicked(MapPanel panel, Node node) {
@@ -194,7 +201,7 @@ public class MainWindowController implements NodeListener {
                 //if the node contains a delivery activate the "supprimer" button and maj delivery info panel
                 mainWindow.featureAddSetEnable(false);
                 mainWindow.featureDeleteSetEnable(true);
-                mainWindow.getRightPanel().getDeliveryInfoPanel().fillDeliveryInfoPanel(del);
+                mainWindow.getRightPanel().getDeliveryInfoPanel().fillDeliveryInfoPanel(del,mainWindow.getCalculatedRound());
             }
         }
     }

ids/src/Model/ChocoSolver/CalculatedRound.java
@@ -252,7 +252,7 @@ public class CalculatedRound {
     }
 
 
-    // TODO: to be tested!
+    // TODO: to be tested!  to be prune !!!
     /**
      * Parse the round into a html format
      * @return the html text in a String
@@ -311,5 +311,21 @@ public class CalculatedRound {
         html += htmlClose;
 
         return html;
+    }
+
+    /**
+     * convert from MILISECONDS (thank you remi) to hours:minutes
+     * @param milisec delay in MILISECONDS (for tyrant supervisor)
+     * @return a string containing hours:minutes (yes we loose some precision)
+     */
+    public static String conversionMSHM(long milisec)
+    {
+        String res="";
+        long hour, min;
+
+        hour = milisec/3600000;
+        min = (milisec-(hour*3600000))/60000;
+        res += hour + ":" + min;
+        return res;
     }
 }

ids/src/Model/ChocoSolver/ChocoGraph.java
@@ -355,12 +355,12 @@ public class ChocoGraph implements Graph {
 	}
 
 	public int[] getSucc(int i) {
-		return deliveries.get(i).getSuccessorsNode();
+		return deliveries.get(nodesId.get(i)).getSuccessorsNode();
 	}
 
 
 	public int getNbSucc(int i) {
-        return deliveries.get(i).getSuccessorsNode().length;
+        return deliveries.get(nodesId.get(i)).getSuccessorsNode().length;
 	}
 
 }

ids/src/View/MainWindow/DeliveryInfoPanel.java
@@ -1,6 +1,8 @@
 package View.MainWindow;
 
+import Model.ChocoSolver.CalculatedRound;
 import Model.Delivery.Delivery;
+import Model.Delivery.Round;
 import View.TimeFramePanel;
 
 import javax.swing.*;
@@ -110,15 +112,19 @@ public class DeliveryInfoPanel extends JPanel {
      * fill all the fields with the information in the Delivery given
      * @param delivery the delivery which should be described
      */
-     public void fillDeliveryInfoPanel(Delivery delivery){
-        deliveryID.setText(""+delivery.getId());
-        clientName.setText(""+delivery.getClient().getId());
-        address.setText((""+delivery.getAddress().getId()));
-        SimpleDateFormat form = new SimpleDateFormat("hh");
-        timeFrameBegin.setText(""+form.format(delivery.getSchedule().getEarliestBound().getTime()));
-        timeFrameEnd.setText(""+form.format(delivery.getSchedule().getLatestBound().getTime()));
-        deliveryTime.setText(" unable to do it now ");
-        delay.setText(" unable to do it now");
+     public void fillDeliveryInfoPanel(Delivery delivery, CalculatedRound round){
+
+            deliveryID.setText(""+delivery.getId());
+            clientName.setText(""+delivery.getClient().getId());
+            address.setText((""+delivery.getAddress().getId()));
+            SimpleDateFormat form = new SimpleDateFormat("hh");
+            timeFrameBegin.setText(""+form.format(delivery.getSchedule().getEarliestBound().getTime()));
+            timeFrameEnd.setText(""+form.format(delivery.getSchedule().getLatestBound().getTime()));
+         if(round != null)
+         {
+            deliveryTime.setText(""+form.format(round.getEstimatedSchedules(delivery).getTime()));
+            delay.setText(""+round.getDelay(delivery));
+         }
      }
 
     /**
@@ -130,6 +136,14 @@ public class DeliveryInfoPanel extends JPanel {
         address.setText("");
         timeFrameBegin.setText("");
         timeFrameEnd.setText("");
+        deliveryTime.setText("");
+        delay.setText("");
+    }
+
+    /**
+     * empty the fields  concerned by a round
+     */
+    public void emptyRoundFields(){
         deliveryTime.setText("");
         delay.setText("");
     }

ids/src/View/MainWindow/RoundPanel.java
@@ -1,8 +1,13 @@
 package View.MainWindow;
 
+import Model.ChocoSolver.CalculatedRound;
+import Model.Delivery.Delivery;
+import Model.Delivery.Round;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.text.SimpleDateFormat;
 
 /**
  * @author H4404 - ABADIE Guillaume, BUISSON Nicolas, CREPET Louise, DOMINGUES Rémi, MARTIN Aline, WETTERWALD Martin
@@ -69,7 +74,7 @@ public class RoundPanel extends JPanel {
         roundPanelContentForm.add(createRow1());
         roundPanelContentForm.add(createRow2());
         roundPanelContentForm.add(createRow3());
-        disableAllFields();
+        setEnableAllFields(false);
 
         // We add this "form" to our roundPanelContent
         roundPanelContent.add(roundPanelContentForm, BorderLayout.CENTER);
@@ -101,12 +106,39 @@ public class RoundPanel extends JPanel {
     }
 
     /**
-     * Disables all JTextFields.
+     * Enable or disable the fields
+     * @param b whether to enable or disable the fields
      */
-    private void disableAllFields() {
-        View.Utils.enableJTextField(deliveryCount, false);
-        View.Utils.enableJTextField(duration, false);
-        View.Utils.enableJTextField(delay, false);
-        View.Utils.enableJTextField(distance, false);
+    private void setEnableAllFields(boolean b) {
+        View.Utils.enableJTextField(deliveryCount, b);
+        View.Utils.enableJTextField(duration, b);
+        View.Utils.enableJTextField(delay, b);
+        View.Utils.enableJTextField(distance, b);
     }
+
+    /**
+     * fill all the fields with the information in the Delivery given
+     * @param round the round which should be described
+     */
+    public void fillRoundPanel(CalculatedRound round){
+        if(round != null)
+        {
+            this.delay.setText(CalculatedRound.conversionMSHM(round.getCumulatedDelay()));
+            this.distance.setText(""+round.getTotalLength());
+            // -1 because the warehouse is two times in this list
+            this.deliveryCount.setText(""+(round.getOrderedItineraries().size()-1));
+            this.duration.setText(CalculatedRound.conversionMSHM(round.getTotalDuration()));
+        }
+    }
+
+    /**
+     * empty all the fields
+     */
+    public void emptyFields(){
+        this.delay.setText("");
+        this.distance.setText("");
+        this.deliveryCount.setText("");
+        this.duration.setText("");
+    }
+
 }

commit 898f023e897bee10f92d8f8220acb57051134c5a

Fixes load round error introduced by 4a1e04e7 and 007738e0.

ids/src/Controller/MainWindowController.java
@@ -83,16 +83,17 @@ public class MainWindowController implements NodeListener {
         if(xmlFile != null) {
             try {
                 mainWindow.setRound(Round.createFromXml(xmlFile.getAbsolutePath(), mainWindow.getNetwork()));
+                computeRound(this.getMainWindow().getNetwork(), this.getMainWindow().getRound());
+                mainWindow.getRightPanel().getRoundPanel().emptyFields();
+                mainWindow.getRightPanel().getDeliveryInfoPanel().emptyFields();
+                mainWindow.getMapPanel().setSelectedNode(null);
+                mainWindow.getRightPanel().getRoundPanel().fillRoundPanel(this.mainWindow.getCalculatedRound());
+
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(mainWindow, "Il y a eu une erreur lors du chargement de la tournée.\n" +
                         e.getMessage(), "Erreur lors du chargement de la tournée", JOptionPane.ERROR_MESSAGE);
             }
         }
-        computeRound(this.getMainWindow().getNetwork(), this.getMainWindow().getRound());
-        mainWindow.getRightPanel().getRoundPanel().emptyFields();
-        mainWindow.getRightPanel().getDeliveryInfoPanel().emptyFields();
-        mainWindow.getMapPanel().setSelectedNode(null);
-        mainWindow.getRightPanel().getRoundPanel().fillRoundPanel(this.mainWindow.getCalculatedRound());
     }
 
     /**