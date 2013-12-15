17cdfc5b35e08a0a35f747af60f56c5128977468
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-15 01:06:33 +0100

    Uses the command key instead of the control key for event mask on Mac OS X

--- ids/src/View/MainWindow/TopMenuBar.java
+++ ids/src/View/MainWindow/TopMenuBar.java
@@ -3,6 +3,7 @@ package View.MainWindow;
 import Controller.MainWindowController;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -98,17 +99,19 @@ public class TopMenuBar extends JMenuBar{
         JMenu file = new JMenu("Fichier");
         file.setMnemonic(KeyEvent.VK_F);
 
+        int eventMask = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
         fileExit = new JMenuItem("Quitter", new ImageIcon(getClass().getResource("./../../img/door_out.png")));
-        fileExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+        fileExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, eventMask));
 
         openMap = new JMenuItem("Ouvrir une carte", new ImageIcon(getClass().getResource("../../img/mini_open_map.png")));
-        openMap.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        openMap.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, eventMask));
 
         openRound = new JMenuItem("Ouvrir une tournée", new ImageIcon(getClass().getResource("../../img/mini_open_round.png")));
-        openRound.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, ActionEvent.CTRL_MASK));
+        openRound.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, eventMask));
 
         saveRound = new JMenuItem("Sauvegarder une tournée", new ImageIcon(getClass().getResource("../../img/mini_save_round.png")));
-        saveRound.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        saveRound.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, eventMask));
 
         file.add(openMap);
         file.add(openRound);
@@ -127,16 +130,18 @@ public class TopMenuBar extends JMenuBar{
         JMenu edit = new JMenu("Éditer");
         edit.setMnemonic(KeyEvent.VK_E);
 
+        int eventMask = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
         addButton = new JMenuItem("Ajouter une livraison", new ImageIcon(getClass().getResource("../../img/mini_add.png")));
 
         delButton = new JMenuItem("Supprimer une livraison", new ImageIcon(getClass().getResource("../../img/mini_delete.png")));
         delButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
 
         undoButton = new JMenuItem("Annuler", new ImageIcon(getClass().getResource("../../img/mini_undo.png")));
-        undoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
+        undoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, eventMask));
 
         redoButton = new JMenuItem("Refaire", new ImageIcon(getClass().getResource("../../img/mini_redo.png")));
-        redoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));
+        redoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, eventMask));
 
         edit.add(addButton);
         edit.add(delButton);
